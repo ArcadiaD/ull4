c6223a452f17819e36e9a3e885d2c996a36b349b
fjpacymper <fjpa@cymper.com>
2016-10-20 16:22:37 +0100

    Update char.js

--- char.js
+++ char.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   var data = {
-    labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
-    series: [{ 
-               data: [2, 4, 6, 8, 10, 12]
-            }]
-  };
+  labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+  series: [{ data: [2, 4, 14, 8, 10, 12] }, // ct-series-a
+           { data: [1, 2, 3, 4, 5, 6] },   // ct-series-b
+           { data: [12, 10,26, 6, 4, 2] }  // ct-series-c
+          ]
+};
 
-  var options = {}
+var options = {}
 
-  new Chartist.Line('.chart', data, options);
+new Chartist.Line('.chart', data, options);
+
 });
