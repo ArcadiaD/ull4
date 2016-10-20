2b7a75e6cee803e4bdc06d3845c969029d042980
fjpacymper <fjpa@cymper.com>
2016-10-20 19:33:08 +0100

    Update panorama.js

--- panorama.js
+++ panorama.js
@@ -1,6 +1,6 @@
 // Javascript
 var PSV = PhotoSphereViewer({
   panorama:  'pano1.jpg',
-  container: 'ID_DEL_DIV_DONDE_IRA_LA_IMAGEN',
+  container: 'photosphere',
   loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
 });
