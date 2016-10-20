38b35546e784f3c6e916a173c8adf5b11ccf4075
fjpacymper <fjpa@cymper.com>
2016-10-20 17:10:41 +0100

    Create viz.js

---
+++ viz.js
@@ -0,0 +1,12 @@
+var sample = 'digraph g { a -> b; }';
+
+var options = {
+  format: 'svg'
+  // format: 'png-image-element'
+}
+
+var image = Viz(sample, options);
+var main = document.getElementById('main');
+
+main.innerHTML = image;        // SVG
+main.appendChild(image);    // PNG
