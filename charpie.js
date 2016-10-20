2da1f018a08dd28d1e60b2ffdfa842f0ae5097de
fjpacymper <fjpa@cymper.com>
2016-10-20 16:38:01 +0100

    Update charpie.js

--- charpie.js
+++ charpie.js
@@ -1,12 +1,14 @@
-var data = {
-  series: [20, 10, 30, 40]
-}
+$(document).ready(function() {
+    var data = {
+      series: [20, 10, 30, 40]
+    }
 
-var options = {
-  // donut: true,
-  // donutWidth: 40,
-  // startAngle: 270,
-  // total: 200
-}
+    var options = {
+      // donut: true,
+      // donutWidth: 40,
+      // startAngle: 270,
+      // total: 200
+    }
 
-new Chartist.Pie('.chart', data, options);
+    new Chartist.Pie('.chart', data, options);
+});
