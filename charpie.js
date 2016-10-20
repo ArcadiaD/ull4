fac9ac1c18bad6262cc68fd3682ec6a2e9543007
fjpacymper <fjpa@cymper.com>
2016-10-20 16:34:13 +0100

    Create charpie.js

---
+++ charpie.js
@@ -0,0 +1,12 @@
+var data = {
+  series: [20, 10, 30, 40]
+}
+
+var options = {
+  // donut: true,
+  // donutWidth: 40,
+  // startAngle: 270,
+  // total: 200
+}
+
+new Chartist.Pie('.chart', data, options);
